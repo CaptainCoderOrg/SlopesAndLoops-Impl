ae94c74f79b95740d19363c3787debe25f9d97d9
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-05-23 21:12:40 -0700

    feat: Fix velocity to follow direction of travel.

--- Slopes Unity 2022/Assets/Scripts/GroundedInfo.cs
+++ Slopes Unity 2022/Assets/Scripts/GroundedInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -8,11 +9,13 @@ public class GroundedInfo : MonoBehaviour
 {
     public LayerMask GroundLayer;
     public float Distance = 1;
-    public Collider2D PlayerCollider;
     public bool IsGrounded = true;
+    public Vector2 Up { get; private set; } = Vector2.up;
+    public Vector2 Left { get; private set; } = Vector2.left;
+
+    private float _movementDirection = -1;
     private Vector2 _previousUp = Vector2.up;
-    public Vector2 Up = Vector2.up;
-    public Vector2 Left = Vector2.left;
+    private Collider2D _collider;
     private Rigidbody2D _rigidbody;
 
     public void Clear()
@@ -25,6 +28,7 @@ public class GroundedInfo : MonoBehaviour
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -37,7 +41,7 @@ public class GroundedInfo : MonoBehaviour
         {
             Up = hit.normal;
             Left = Quaternion.Euler(0, 0, 90) * Up;
-            ColliderDistance2D distance = hit.collider.Distance(PlayerCollider);
+            ColliderDistance2D distance = hit.collider.Distance(_collider);
             SnapToGround(distance);
         }
         else
@@ -46,7 +50,7 @@ public class GroundedInfo : MonoBehaviour
             IsGrounded = false;
         }
         Debug.DrawRay(transform.position, Up, Color.cyan, 1);
-        Debug.DrawRay(transform.position, Left, Color.green, 1);
+        Debug.DrawRay(transform.position, _rigidbody.velocity, Color.green);
     }
 
     private bool SnapToGround(ColliderDistance2D distance)
@@ -61,7 +65,11 @@ public class GroundedInfo : MonoBehaviour
             Debug.DrawLine(distance.pointA, distance.pointB, Color.red, 1);
             Vector3 toMove = distance.normal * distance.distance;
             transform.position = transform.position + toMove;
-            _rigidbody.velocity = Quaternion.Euler(0, 0, Vector2.SignedAngle(_previousUp, Up)) * _rigidbody.velocity;
+            if (Math.Abs(_rigidbody.velocity.x) > 0.01)
+            {
+                _movementDirection = Mathf.Sign(_rigidbody.velocity.x);
+            }
+            _rigidbody.velocity = _rigidbody.velocity.magnitude * _movementDirection * -Left;
             return true;
         }
         return false;
