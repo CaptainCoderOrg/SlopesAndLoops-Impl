c948e01dacd0667d67d383a3c4829569f67ade4b
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-05-26 11:27:28 -0700

    feat: Add IsVertical to fall condition.

--- Slopes Unity 2022/Assets/Scripts/SlopedGroundController.cs
+++ Slopes Unity 2022/Assets/Scripts/SlopedGroundController.cs
@@ -11,26 +11,15 @@ public class SlopedGroundController : MonoBehaviour
     private float _colliderHeight;
     public bool IsGrounded = true;
     public float FallThreshold = 2;
+    public float TargetMomentum = -1;
+    // The player is considered upside down if their "Up" is within 15 degrees of world down
+    public bool IsUpsideDown => Vector2.Angle(Up, Vector2.down) < 15;
+    // The player is considered vertical if they are moving beyond a 90 degree angle 
+    public bool IsVertical => Vector2.Angle(Up, Vector2.down) <= 90;
     public Vector2 Up { get; private set; } = Vector2.up;
     public Vector2 Right { get; private set; } = Vector2.right;
-    public float TargetMomentum = -1;
     private Collider2D _collider;
     private Rigidbody2D _rigidbody;
-
-    public void UpdateTargetMomentum(float targetMomentum)
-    {
-        bool isStoppingOrTurning = SignOrZero(TargetMomentum) != SignOrZero(targetMomentum);
-        if (isStoppingOrTurning) { ClearGround(); }
-        TargetMomentum = SignOrZero(targetMomentum);
-    }
-
-    public void ClearGround()
-    {
-        Up = Vector2.up;
-        Right = Vector2.right;
-        transform.rotation = Quaternion.Euler(0, 0, 0);
-    }
-
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -40,11 +29,19 @@ public class SlopedGroundController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (_rigidbody.velocity.magnitude < FallThreshold) { ClearGround(); }
+        // If the player is If the player is not moving fast enough, they fall off walls and steep slopes
+        if (IsVertical && _rigidbody.velocity.magnitude < FallThreshold) { ClearGround(); }
+
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -Up, _colliderHeight + SnapDistance, _groundLayer);
-        if (!IsUpsideDown) { SnapTo(hit.collider); }
-        IsGrounded = hit.collider != null;        
+        // Use raycast to determine if we are touching the ground
+        IsGrounded = hit.collider != null;
+
+        // If the player is touching the ground and is not upside down, snap them to the ground
+        if (IsGrounded && !IsUpsideDown) { SnapTo(hit.collider); }
+
+        // If the player is touching the ground, adjust the player's Up direction to match the surface of the ground
         if (IsGrounded) { SetPlayerUp(hit.normal); }
+        // Otherwise, clear the ground
         else { ClearGround(); }
 
         // Draw Debug information in Scene View
@@ -55,8 +52,25 @@ public class SlopedGroundController : MonoBehaviour
             Debug.DrawRay(transform.position, _rigidbody.velocity * .5f, Color.green);
         }
     }
-    private bool IsUpsideDown => Vector2.Angle(Up, Vector2.down) < 15;
-    private int SignOrZero(float num) => num == 0 ? 0 : num > 0 ? 1 : -1;
+
+    public void UpdateTargetMomentum(float targetMomentum)
+    {
+        // If the incoming momentum is different than the previous direction, the player is either stopping or turning
+        // and clear their ground connection. This allows the player to turn off walls rather than changing directions
+        // when they are running up / down a wall.
+        bool isStoppingOrTurning = TargetMomentum != SignOrZero(targetMomentum);
+        if (isStoppingOrTurning) { ClearGround(); }
+        TargetMomentum = SignOrZero(targetMomentum);
+    }
+
+    public void ClearGround()
+    {
+        // Reset the player's Up and Right
+        Up = Vector2.up;
+        Right = Vector2.right;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
     private void SetPlayerUp(Vector2 newUp)
     {
         const float rotateThreshold = 80;
@@ -78,19 +92,14 @@ public class SlopedGroundController : MonoBehaviour
 
     private void SnapTo(Collider2D ground)
     {
-        // If there is no ground, clear the ground
-        if (ground == null) { ClearGround(); }
-        // If we are upside down, do not snap
-        else if (IsUpsideDown) { return; }
-        else
-        {
-            // Find the distance to the ground
-            ColliderDistance2D distance = ground.Distance(_collider);
-            // If we are already touching the ground, do nothing
-            if (distance.isOverlapped) { return; }
-            // Otherwise, move the player to the collider
-            transform.position += (Vector3)(distance.normal * distance.distance);
-            if (ShowDebugInfo) { Debug.DrawLine(distance.pointA, distance.pointB, Color.red, 1); }
-        }
+        // Find the distance to the ground
+        ColliderDistance2D distance = ground.Distance(_collider);
+        // If we are already touching the ground, do nothing
+        if (distance.isOverlapped) { return; }
+        // Otherwise, move the player to the collider
+        transform.position += (Vector3)(distance.normal * distance.distance);
     }
+
+    // Helper function for determining if a float is positive, negative, or zero
+    private static int SignOrZero(float num) => num == 0 ? 0 : num > 0 ? 1 : -1;
 }
